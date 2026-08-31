03c3df99d50925ac2a6ca27f256c794de41c5e1d
lyhjoy90 <lyhjoy90@gmail.com>
2026-08-31 15:27:49 +0900

    8월 31일 15시 20분

--- src/main/java/com/basic/admin/MySpringBootAdminApplication.java
+++ src/main/java/com/basic/admin/MySpringBootAdminApplication.java
@@ -1,15 +1,23 @@
 package com.basic.admin;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableAdminServer
 public class MySpringBootAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootAdminApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringBootAdminApplication.class);
+
+		// 아래 줄을 주석 처리하거나 삭제합니다.
+		// application.setWebApplicationType(WebApplicationType.SERVLET);
+
+		application.run(args);
 	}
 
+	@Bean
+	public String myBean() {
+		return "Hello Bean";
+	}
 }
