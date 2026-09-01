cba2a702682c03756df6036023924e67933a177d
lyhjoy90 <lyhjoy90@gmail.com>
2026-09-01 14:52:21 +0900

    '0831후번0901놓침다시하기'

---
+++ src/main/java/com/basic/admin/entitiy/Customer.java
@@ -0,0 +1,20 @@
+package com.basic.admin.entitiy;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter @Setter
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String customerName;
+
+}
