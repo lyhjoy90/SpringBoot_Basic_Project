48dedaf66bf6cd8f7809663b76a2d6fcef5594ca
lyhjoy90 <lyhjoy90@gmail.com>
2026-09-06 18:44:54 +0900

    0902 7교시 postman defaultexceptionadvice

--- src/main/java/com/basic/admin/entity/Customer.java
+++ src/main/java/com/basic/admin/entity/Customer.java
@@ -4,11 +4,17 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "customers")
+@DynamicUpdate
 @Getter @Setter
 public class Customer {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
@@ -16,5 +22,9 @@ public class Customer {
 
     @Column(nullable = false)
     private String customerName;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
 
 }
---
+++ src/main/java/com/basic/admin/entity/User.java
@@ -0,0 +1,34 @@
+package com.basic.admin.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Name(이름)은 필수 입력 항목입니다.")
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email 주소는 필수 입력 항목입니다.")
+    @Email
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
